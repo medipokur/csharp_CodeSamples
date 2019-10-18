3add2fe922526298b9480ba661d5a884c334fa6d
medipokur <48018034+medipokur@users.noreply.github.com>
2019-10-18 16:05:01 +0300

    BinCleaner designer
    designer dosyası

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace BinCleaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cleanButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.selectButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cleanButton
+            // 
+            this.cleanButton.Location = new System.Drawing.Point(908, 172);
+            this.cleanButton.Name = "cleanButton";
+            this.cleanButton.Size = new System.Drawing.Size(120, 52);
+            this.cleanButton.TabIndex = 0;
+            this.cleanButton.Text = "Temizle";
+            this.cleanButton.UseVisualStyleBackColor = true;
+            this.cleanButton.Click += new System.EventHandler(this.cleanButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(68, 180);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(647, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // selectButton
+            // 
+            this.selectButton.Location = new System.Drawing.Point(737, 172);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(103, 42);
+            this.selectButton.TabIndex = 2;
+            this.selectButton.Text = "Klasör Seç";
+            this.selectButton.UseVisualStyleBackColor = true;
+            this.selectButton.Click += new System.EventHandler(this.selectButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1155, 516);
+            this.Controls.Add(this.selectButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cleanButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cleanButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button selectButton;
+    }
+}
+
