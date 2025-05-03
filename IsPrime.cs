0e20997005635bf1b1db7ad99dd595022e884866
medipokur <48018034+medipokur@users.noreply.github.com>
2025-05-04 00:07:20 +0300

    Update IsPrime.cs
    performance optimization

--- IsPrime.cs
+++ IsPrime.cs
@@ -8,7 +8,7 @@ public bool IsPrime(int number){
 
 	if (number <= 1) return false;
 	
-	for (int i=2; i <= number/2; ++i)
+	for (int i=2; i <= Math.Sqrt(number); ++i)
 	{
 		if (number % i == 0) return false;	
 	}
